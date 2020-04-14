f0383fa4ba27b26cbc52ce061a0a54682fe9d461
Somya Guglani <somyaguglani0897@gmail.com>
2020-04-14 15:55:43 +0530

    made grid with some errors present

--- browser/index.js
+++ browser/index.js
@@ -1,8 +1,10 @@
 import Node from "../browser/Node.js";
 
 const gridContainer = document.querySelector(`.grid`);
-console.log(gridContainer);
 
+const navbarContainer = document.querySelector(`.navbarContainer`);
+let navHeight = navbarContainer.offsetHeight;
+const mainContentContainer = document.querySelector(`.mainContentContainer`);
 //---------------FUNCTIONS---------------
 function Board(width, height) {
   this.width = width;
@@ -18,29 +20,31 @@ function Board(width, height) {
 
 Board.prototype.initialize = function () {
   this.createGrid();
-  this.addEventListenters();
-  this.tutorialWork();
+  // this.addEventListenters();
+  // this.tutorialWork();
 };
 
 Board.prototype.createGrid = function () {
   let htmlOfGrid = ``;
-  for (let row = 0; row < this.width; row++) {
-    let gridRow = `<tr>`;
+  for (let row = 0; row < this.height; row++) {
+    let gridRow = `<tr id = "row_${row}">`;
     const allNodesRowArray = [];
-    for (let col = 0; col < this.height; col++) {
+    for (let col = 0; col < this.width; col++) {
       //the node can be a start node or target node or unvisited node
 
       const id = `${row}-${col}`;
       let nodeStatus;
       if (
         row === Math.floor(this.height / 2) &&
-        col === Math.floor(this.width / 4)
+        col === Math.floor(this.width / 4) &&
+        this.start.length === 0
       ) {
         nodeStatus = `start`;
         this.start = `${id}`;
       } else if (
         row === Math.floor(this.height / 2) &&
-        col === 3 * Math.floor(this.width / 4)
+        col === Math.floor((3 * this.width) / 4) &&
+        this.target.length === 0
       ) {
         nodeStatus = `target`;
         this.target = `${id}`;
@@ -49,15 +53,25 @@ Board.prototype.createGrid = function () {
       }
       const newNode = new Node(id, nodeStatus);
       allNodesRowArray.push(newNode);
-      girdRow += `<td id = "${id}"class = "${nodeStatus}"></td>`;
+      gridRow += `<td id = "${id}"class = "${nodeStatus}"></td>`;
     }
-    gridRow += `</tr>`;
+    htmlOfGrid += `${gridRow}</tr>`;
     this.allNodesArray.push(allNodesRowArray);
     htmlOfGrid += gridRow;
   }
   gridContainer.innerHTML = htmlOfGrid;
+
   console.log(this.allNodesArray);
-  console.log(htmlOfGrid);
 };
 
 //----------------MAKING BOARD OBJECT-------------
+
+let contentHeight = mainContentContainer.offsetHeight;
+let docHeight = document.documentElement.scrollHeight;
+let docWidth = document.documentElement.scrollWidth;
+let height = Math.floor((docHeight - contentHeight - navHeight) / 53);
+let width = Math.floor(docWidth / 25);
+let board = new Board(width, height);
+console.log(width, height);
+// console.log(board);
+board.initialize();
