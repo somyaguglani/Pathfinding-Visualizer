d05e62016f26b087ce483c8d364b7f357dbb9539
Somya Guglani <somyaguglani0897@gmail.com>
2020-04-18 23:08:14 +0530

    css done

--- browser/Node.js
+++ browser/Node.js
@@ -1,6 +1,7 @@
 function Node(id, status) {
   this.id = id;
   this.status = status;
+  this.weight = 0;
 }
 
 export default Node;
--- browser/index.js
+++ browser/index.js
@@ -1,7 +1,6 @@
 import Node from "../browser/Node.js";
 
 const gridContainer = document.querySelector(`.grid`);
-
 const navbarContainer = document.querySelector(`.navbarContainer`);
 let navHeight = navbarContainer.offsetHeight;
 const mainContentContainer = document.querySelector(`.mainContentContainer`);
@@ -17,11 +16,13 @@ function Board(width, height) {
   this.start = ``;
   this.target = ``;
   this.allNodesArray = [];
+  this.tutorialContentArray = [];
 }
 
 Board.prototype.initialize = function () {
+  this.contentInitialize();
   this.createGrid();
-  // this.addEventListenters();
+  this.addEventListeners();
   this.tutorialWork();
 };
 
@@ -62,10 +63,48 @@ Board.prototype.createGrid = function () {
   gridContainer.innerHTML = htmlOfGrid;
 };
 
+Board.prototype.addEventListeners = function () {
+  const board = this;
+  for (let row = 0; row < board.height; row++) {
+    for (let col = 0; col < board.width; col++) {
+      const currentNodeId = `${row}-${col}`;
+      const currentNodeElement = document.getElementById(`${currentNodeId}`);
+      const currentNode = board.getNode(currentNodeId);
+      currentNodeElement.onmousedown = (e) => {
+        e.preventDefault();
+        if (currentNode.status === `start` || currentNode.status === `target`) {
+          console.log(`start or target`);
+        } else {
+          console.log(`other node`);
+          board.changeNodeStatus(currentNode, currentNodeElement);
+        }
+      };
+    }
+  }
+};
+
+Board.prototype.changeNodeStatus = function (currentNode, currentNodeElement) {
+  const unweightedalgos = [`dfs`, `bfs`];
+  console.log(currentNode, currentNodeElement);
+  const specialStatus = [`start`, `target`];
+  if (!specialStatus.includes(currentNode.status)) {
+    currentNodeElement.classList.toggle(
+      currentNode.status === `unvisited` ? `wall` : `unvisited`
+    );
+    currentNode.status = currentNodeElement.classList[0];
+  }
+  //weights
+};
+
+Board.prototype.getNode = function (id) {
+  const [i, j] = id.split(`-`);
+  return this.allNodesArray[i][j];
+};
 let counter = 0;
-const contentArray = [];
-contentArray.push(
-  `<h1>Welcome to Pathfinding Visualizer!</h1>
+
+Board.prototype.contentInitialize = function () {
+  this.tutorialContentArray.push(
+    `<h1>Welcome to Pathfinding Visualizer!</h1>
         <h2>
           This short tutorial will walk you through all of the features of this
           application.
@@ -74,7 +113,7 @@ contentArray.push(
           If you want to dive right in, feel free to press the "Skip Tutorial"
           button below. Otherwise, press "Next"!
         </p>
-        <div class="pageCounter">${counter + 1}/9</div>
+        <div class="pageCounter">1/9</div>
         <p>If you want to see the source code for this application, check out my <a  href="https://github.com/somyaguglani/Pathfinding-Visualizer" >github </a></p>
         <img style=" height:150px;" src="./styling/imagesAndSvg/c_icon.png" alt="startingIcon">
 
@@ -83,36 +122,36 @@ contentArray.push(
           <button class="prevButton">Previous</button>
           <button class="nextButton">Next</button>
         </div>`
-);
-contentArray.push(`<h1>What is a pathfinding algorithm?</h1>
+  );
+  this.tutorialContentArray.push(`<h1>What is a pathfinding algorithm?</h1>
         <h2>
         At its core, a pathfinding algorithm seeks to find the shortest path between two points. This application visualizes various pathfinding algorithms in action, and more!
         </h2>
         <p>
         All of the algorithms on this application are adapted for a 2D grid, where 90 degree turns have a "cost" of 1 and movements from a node to another have a "cost" of 1.
         </p>
-        <div class="pageCounter">${counter + 1}/9</div>
+        <div class="pageCounter">2/9</div>
         <img src="./styling/imagesAndSvg/path.png" alt="path">
         <div class="tutorialButtons">
           <button class="skipButton">Skip Tutorial</button>
           <button class="prevButton">Previous</button>
           <button class="nextButton">Next</button>
         </div>`);
-contentArray.push(`<h1>Picking an algorithm</h1>
+  this.tutorialContentArray.push(`<h1>Picking an algorithm</h1>
         <h2>
         Choose an algorithm from the "Algorithms" drop-down menu.
         </h2>
         <p>
        Note that some algorithms are <strong>unweighted</strong>, while others are <strong>weighted</strong>. Unweighted algorithms do not take turns or weight nodes into account, whereas weighted ones do. Additionally, not all algorithms guarantee the shortest path.
         </p>
-        <div class="pageCounter">${counter + 1}/9</div>
+        <div class="pageCounter">3/9</div>
         <img src="./styling/imagesAndSvg/algorithms.png" alt="algoDemo">
         <div class="tutorialButtons">
           <button class="skipButton">Skip Tutorial</button>
           <button class="prevButton">Previous</button>
           <button class="nextButton">Next</button>
         </div>`);
-contentArray.push(`<h1>Meet the algorithms</h1>
+  this.tutorialContentArray.push(`<h1>Meet the algorithms</h1>
         <h2>
        Not all algorithms are created equal.
         </h2>
@@ -133,55 +172,56 @@ contentArray.push(`<h1>Meet the algorithms</h1>
  </br>
  <strong>Depth-first Search </strong> (unweighted): a very bad algorithm for pathfinding; does not guarantee the shortest path
         </p>
-        <div class="pageCounter">${counter + 1}/9</div>
+        <div class="pageCounter">4/9</div>
         <div class="tutorialButtons">
           <button class="skipButton">Skip Tutorial</button>
           <button class="prevButton">Previous</button>
           <button class="nextButton">Next</button>
         </div>`);
-contentArray.push(`<h1>Adding walls and weights</h1>
+  this.tutorialContentArray.push(`<h1>Adding walls and weights</h1>
         <h3>
        Click on the grid to add a wall. Click on the grid while pressing W to add a weight. Generate mazes and patterns from the "Mazes & Patterns" drop-down menu.
         </h3>
         <p>
        Walls are impenetrable, meaning that a path cannot cross through them. Weights, however, are not impassable. They are simply more "costly" to move through. In this application, moving through a weight node has a "cost" of 15.
         </p>
-        <div class="pageCounter">${counter + 1}/9</div>
+        <div class="pageCounter">5/9</div>
         <img src="./styling/imagesAndSvg/walls.gif" alt="wallsDemo">
         <div class="tutorialButtons">
           <button class="skipButton">Skip Tutorial</button>
           <button class="prevButton">Previous</button>
           <button class="nextButton">Next</button>
         </div>`);
-contentArray.push(`<h1>Dragging nodes</h1>
+  this.tutorialContentArray.push(`<h1>Dragging nodes</h1>
         <h2>
     Click and drag the start, bomb, and target nodes to move them.
         </h2>
         <p>
       Note that you can drag nodes even after an algorithm has finished running. This will allow you to instantly see different paths.
         </p>
-        <div class="pageCounter">${counter + 1}/9</div>
+        <div class="pageCounter">6/9</div>
         <img src="./styling/imagesAndSvg/dragging.gif" alt="draggingDemo">
         <div class="tutorialButtons">
           <button class="skipButton">Skip Tutorial</button>
           <button class="prevButton">Previous</button>
           <button class="nextButton">Next</button>
         </div>`);
-contentArray.push(`<h1>Visualizing and more</h1>
+  this.tutorialContentArray.push(`<h1>Visualizing and more</h1>
         <h2>
        Use the navbar buttons to visualize algorithms and to do other stuff!
         </h2>
         <p>
       You can clear the current path, clear walls and weights, clear the entire board, and adjust the visualization speed, all from the navbar. If you want to access this tutorial again, click on "Pathfinding Visualizer" in the top left corner of your screen.
         </p>
-        <div class="pageCounter">${counter + 1}/9</div>
-        <img src="./styling/imagesAndSvg/navbar.png" alt="algoDemo">
-        <h3>Now it's time to play around with the visualizer. Enjoy!</h3>
+        <div class="pageCounter">7/9</div>
+        <img class = "responsive-img" src="./styling/imagesAndSvg/navbar.png" alt="algoDemo">
+        <h3>Now it's time to play around with the visualizer. I hope you have as much fun as i had building it. Enjoy!</h3>
         <div class="tutorialButtons">
           <button class="skipButton">Skip Tutorial</button>
           <button class="prevButton">Previous</button>
           <button class="nextButton">Finish</button>
         </div>`);
+};
 
 Board.prototype.tutorialWork = function () {
   const board = this;
@@ -193,20 +233,22 @@ Board.prototype.tutorialWork = function () {
     button.addEventListener(`click`, function (e) {
       if (e.currentTarget.classList.value === `skipButton`) {
         modal.style.display = `none`;
+        board.toggleButtons();
       } else if (e.currentTarget.classList.value === `prevButton`) {
         if (counter > 0) {
           counter--;
-          modalInner.innerHTML = contentArray[counter];
+          modalInner.innerHTML = board.tutorialContentArray[counter];
           board.tutorialWork();
         }
       } else if (e.currentTarget.classList.value === `nextButton`) {
-        if (counter === contentArray.length - 1) {
+        if (counter === board.tutorialContentArray.length - 1) {
           modal.style.display = `none`;
+          board.toggleButtons();
           return;
         }
-        if (counter < contentArray.length) {
+        if (counter < board.tutorialContentArray.length) {
           counter++;
-          modalInner.innerHTML = contentArray[counter];
+          modalInner.innerHTML = board.tutorialContentArray[counter];
           board.tutorialWork();
         }
       }
@@ -214,13 +256,18 @@ Board.prototype.tutorialWork = function () {
   });
 };
 
+Board.prototype.toggleButtons = function () {
+  console.log(`all buttons function`);
+  //complete this function
+};
+
 //----------------MAKING BOARD OBJECT-------------
 
 let contentHeight = mainContentContainer.offsetHeight;
 let docHeight = document.documentElement.scrollHeight;
 let docWidth = document.documentElement.scrollWidth;
-let height = Math.floor((docHeight - contentHeight - navHeight) / 25);
-let width = Math.floor(docWidth / 24);
+let height = Math.floor((docHeight - contentHeight - navHeight) / 27);
+let width = Math.floor(docWidth / 26);
 let board = new Board(width, height);
 board.initialize();
-//errors-> pic, counter, concepts->event listeners
+//errors-> pic,  concepts->event listeners
